a00eab5a99c1a82fa81ad2c9e719bde227d957ff
A. Göktuğ Yalçın <goktugyalcin06@gmail.com>
2022-01-10 13:30:46 +0300

    Layout update

--- src/root.link.js
+++ src/root.link.js
@@ -1,11 +1,11 @@
 export const RESUME_PATH = 'https://drive.google.com/file/d/1MbvnX4qNVomEsHp11_4d-Fl7sPkhH_pw/view?usp=sharing';
 export const GITHUB_USERNAME = 'yalcingoktug';
 export const LINKEDIN_USERNAME = 'yalcing';
-export const FIRST_NAME = 'A. Göktuğ';
+export const FIRST_NAME = 'Göktuğ';
 export const LAST_NAME = 'Yalçın';
 export const ADDRESS = 'Istanbul, Turkey'
 export const EMAIL_ID = 'goktugyalcin06@gmail.com';
 export const TWITTER_ID = 'ordansutuvar';
-export const FRONT_PAGE_NAME = 'A. Göktuğ Yalçın';
+export const FRONT_PAGE_NAME = 'Göktuğ Yalçın';
 export const FRONT_PAGE_DESC = 'A Frontend Developer in love with solving problems on can be sustain.'
 
